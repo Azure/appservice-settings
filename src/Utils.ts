8d9e329160d147e7206a4c3edf0498d2ceb38d62
aksm-ms <58936966+aksm-ms@users.noreply.github.com>
2020-12-01 12:56:41 +0530

    adding maskInputs (#18)

---
+++ src/Utils.ts
@@ -0,0 +1,23 @@
+import * as core from '@actions/core';
+
+export class Utils {
+    static validateSettings(customSettings: string, maskInputs: string) {
+        try {
+            var customParsedSettings = JSON.parse(customSettings);
+            if (maskInputs !== "false") {
+                Utils.maskValues(customParsedSettings);
+            }
+            return customParsedSettings;
+        }
+        catch (error) {
+            throw new Error('Given Settings object is not a valid JSON');
+        }
+    }
+    
+    static maskValues(jsonContent: any) {
+        for(let i = 0; i< Object.keys(jsonContent).length; i++) {
+            core.setSecret(jsonContent[i].value);
+        }
+    }
+
+}
